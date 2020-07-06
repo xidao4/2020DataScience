3dfd2a3c28884474d914a256e7c8f4619b1a727f
Pole <2567712208@qq.com>
2020-07-06 22:22:15 +0800

    feat: 题目度量改为多份一起平均

--- src/method1/PDI/cmd_command.py
+++ src/method1/PDI/cmd_command.py
@@ -1,10 +1,10 @@
 import json
 import os
-
+import random
 f=open("s_pro_detail_dict.json",encoding="utf8")
 res=f.read()
 pro_dict=json.loads(res)
-
+target=10  #随机选十份有用代码计算度量值
 
 commands=[]
 pre="metrics\\"
@@ -17,21 +17,31 @@ for v in pro_dict.values():
         os.mkdir(pre+v["case_id"]+"\\hal")
     if not os.path.exists(pre+v["case_id"]+"\\LLOC"):
         os.mkdir(pre+v["case_id"]+"\\LLOC")
+#新增
+    usefulRecord=[]      #存放所有的有用的提交代码路径  包括参考答案和同学的满分答案
+    for i in v["full_records"]:
+        url = "E:\\2020DataScience\\src\\Preprocessing\\" + i + "main_modify.py"
+        usefulRecord.append(url)
+    if len(usefulRecord)>=target:
+        usefulRecord=random.sample(usefulRecord,target)  #随机抽取target份
     if v["ans_is_py"]:
-        url="F:\\2020Data\\200616\\src\\Preprocessing\\"+v["case_unzip_dir"]+".mooctest\\answer_modify.py"
-    elif len(v["full_records"]) != 0:
-        url = "F:\\2020Data\\200616\\src\\Preprocessing\\" + v["full_records"][0] + "main_modify.py"
-    else:
+        if len(usefulRecord)==target:   #满足target个删掉一个让给标准答案
+              usefulRecord=random.sample(usefulRecord,len(usefulRecord)-1)  #一定把是py的参考答案加进去
+        url = "E:\\2020DataScience\\src\\Preprocessing\\" + v["case_unzip_dir"] + ".mooctest\\answer_modify.py"
+        usefulRecord.append(url)
+    if len(usefulRecord)==0:
         continue
-    output_url="F:\\2020Data\\200616\\src\\method1\\PDI\\metrics\\"+v["case_id"]+"\\cc\\cc.json"
-    c="radon cc "+url+" --show-complexity --json --output-file \""+output_url+"\"\n"
-    commands.append(c)
-    output_url="F:\\2020Data\\200616\\src\\method1\\PDI\\metrics\\"+v["case_id"]+"\\hal\\hal.json"
-    c="radon hal "+url+" --json --output-file \""+output_url+"\"\n"
-    commands.append(c)
-    output_url = "F:\\2020Data\\200616\\src\\method1\\PDI\\metrics\\" + v["case_id"] + "\\LLOC\\raw.json"
-    c="radon raw "+url+" --json --output-file \""+output_url+"\"\n"
-    commands.append(c)
+    for u in usefulRecord:
+        url=u
+        output_url="E:\\2020DataScience\\src\\method1\\PDI\\metrics\\"+v["case_id"]+"\\cc\\cc"+str(usefulRecord.index(u))+".json"
+        c="radon cc "+url+" --show-complexity --json --output-file \""+output_url+"\"\n"
+        commands.append(c)
+        output_url="E:\\2020DataScience\\src\\method1\\PDI\\metrics\\"+v["case_id"]+"\\hal\\hal"+str(usefulRecord.index(u))+".json"
+        c="radon hal "+url+" --json --output-file \""+output_url+"\"\n"
+        commands.append(c)
+        output_url = "E:\\2020DataScience\\src\\method1\\PDI\\metrics\\" + v["case_id"] + "\\LLOC\\raw"+str(usefulRecord.index(u))+".json"
+        c="radon raw "+url+" --json --output-file \""+output_url+"\"\n"
+        commands.append(c)
 
 print(commands)
 with open('get_metrics.bat',mode='w') as f:
