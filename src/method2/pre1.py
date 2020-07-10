b6da824ba2e0cdcd83bedd9538288e1bda6852f9
xidao4 <181250086@smail.nju.edu.cn>
2020-07-10 20:38:06 +0800

    feat: 重构

--- src/method2/pre1.py
+++ src/method2/pre1.py
@@ -7,6 +7,11 @@ import pandas as pd
 from pandas import DataFrame
 from pandas import Series
 
+'''
+    选出合适的比赛
+    将选出的比赛的题目ID和分数写进CSV
+'''
+
 
 def get_page_Nums(contest_name,pages):
     url="https://atcoder.jp/contests/"+contest_name+"/submissions"
@@ -59,16 +64,27 @@ def get_new_id(arrLike,contest):
     #     return contest+'_'+id[:idx]
     return contest+'_'+id
 
-def get_difficulty(contest_name):
+def process_id(rows,contest_name):
+    for lst in rows:
+        lst[0]=contest_name+lst[0][0]
+    print('新的ID,eg.  agc004_F')
+    print(rows)
+    return rows
 
-    print("OJ给的题目难度（由题目的分值决定）")
+def check_validation(rows):
+    for lst in rows:
+        if '(' in lst[1]:
+            return False
+    return True
+
+def get_difficulty(contest_name):
     url="https://atcoder.jp/contests/"+contest_name
     try:
         r=requests.get(url,headers = {"User-Agent": "Mozilla//5.0 (Windows NT 10.0; Win64; x64) AppleWebKit//537.36 (KHTML, like Gecko) Chrome//80.0.3987.132 Safari//537.36"})
         print(r.status_code) #状态码
     except Exception as e:
         print(contest_name,'爬取这次比赛的题目难度失败，可能是这场比赛不存在',e)
-        return
+        return False
     html=r.content.decode(r.encoding)  #内容,字符串？
     html=etree.HTML(html)   #转换成HTML格式？
     #print(etree.tostring(html).decode())
@@ -82,28 +98,20 @@ def get_difficulty(contest_name):
         row.append(ret[i])
         row.append(ret[i+1])
         rows.append(row)
-    print('分好组的列表')
+    print('爬取下来的分好组的题目分值')
     print(rows) #分好组的列表
-    filename = "contests\\"+contest_name+"\\pro_score.csv"
+    if not check_validation(rows):
+        return False
+    rows=process_id(rows,contest_name)
+    if not os.path.exists("contests\\" + contest_name):
+        os.mkdir("contests\\" + contest_name)
+    filename = "contests\\"+contest_name+"\\score_"+contest_name+".csv"
     fields=['id','score']
     with open(filename, 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow(fields)
         csvwriter.writerows(rows)
-    #获取modify_score,同时获取带比赛编号的题目名称id
-    data = pd.read_csv(filename)
-    print('变换前')
-    print(data)
-    #data['modify_score'] = data.apply(str_to_int, axis=1)
-    data['id'] = data.apply(get_new_id, axis=1, args=(contest_name,))
-    #del data['score']
-    print('变换后')
-    print(data)
-    data.to_csv("contests\\" + contest_name + "\\pro_score_modify.csv", index=False)# id modify_score
-    print()
-    print()
-    print()
-
+    return True
 
 def get_diff_lvl(arrLike):
     s=arrLike['score']
@@ -152,16 +160,41 @@ if __name__=="__main__":
     for i in range(10,47):
         if i==42: continue #042没有这场比赛
         contests.append("agc0"+str(i))  #010-041 043-046
+
+    #contests=['agc022']
+
+    chosen_contests=[]
+    #获取题目难度，如果不带括号，则加入chosen_contests中，并且新建contests\\agc004\\score_agc004.csv，属性为id(带比赛的编号),score
+    for contest_name in contests:
+        print()
+        if get_difficulty(contest_name):
+            chosen_contests.append(chosen_contests)
+        else:
+            print(contest_name+'不符合条件')
+    print(chosen_contests)
+
+
+
+
+    #创建pro_with_difficulty.csv,根据情况划分难度
+
+    #提高准确率： 换特征值 限制深度 改变难度划分 多些数据 分题型计算准确率
+
+
     #get_total_page_Nums(contests)  #获取全部比赛的页数   page_Nums.csv
-    #get_chosen_page_Nums()  #页数从小到大排序，获取前18场比赛      chosen_page_Nums.csv
-    #选定的比赛的题目分数及相应难度
-    data = pd.read_csv("chosen_page_Nums.csv")
-    contest_series=data.id
-    for i,contest in contest_series.items():
-        print(contest)
-        path="contests\\"+contest
-        if not os.path.exists(path):
-            os.mkdir(path)
-        #选定的比赛的题目的分数
-        #get_difficulty(contest) #选定的比赛的题目的难度
-    get_chosen_pro_info()  #选定的比赛的题目id,modify_score,difficulty_level
+
+
+
+
+    # get_chosen_page_Nums()  #页数从小到大排序，获取前18场比赛      chosen_page_Nums.csv
+    # #选定的比赛的题目分数及相应难度
+    # data = pd.read_csv("chosen_page_Nums.csv")
+    # contest_series=data.id
+    # for i,contest in contest_series.items():
+    #     print(contest)
+    #     path="contests\\"+contest
+    #     if not os.path.exists(path):
+    #         os.mkdir(path)
+    #     #选定的比赛的题目的分数
+    #     #get_difficulty(contest) #选定的比赛的题目的难度
+    # get_chosen_pro_info()  #选定的比赛的题目id,modify_score,difficulty_level
---
+++ src/method2/pre2.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from pandas import DataFrame
+'''
+创建pro.csv    选定的比赛的题目id(带比赛的编号),score
+'''
+
+def get_pro(contests):
+    df0=DataFrame({'id':[]})
+    for contest_name in contests:
+        df=pd.read_csv("contests\\"+contest_name+"\\score_"+contest_name+".csv")
+        df0=pd.merge(df0,df,how='outer')
+    df0.to_csv("pro.csv",index=False)
+
+if __name__=='__main__':
+
+    contests=[3,6,9,10,13,14,15,16,18,23,24,25,26,31,32,36,37,38,40,44,45,46]
+    for i in range(len(contests)):
+        if len(str(contests[i]))==1:
+            contests[i]='agc00'+str(contests[i])
+        else:
+            contests[i]='agc0'+str(contests[i])
+    print(contests)
+    print(len(contests))
+
+    get_pro(contests)
