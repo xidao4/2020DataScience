f5fa2e1390d82a9281eb3a526e9357f24ce8a686
Arceus <3081666963@qq.com>
2020-07-18 09:37:54 +0800

    fix: address structure

--- src/Preprocessing/get_50+50_file.py
+++ src/Preprocessing/get_50+50_file.py
@@ -12,11 +12,13 @@ res = f.read()
 data = json.loads(res)
 data = list(dict.values(data))
 
+
 for rec in data:
     sd = os.path.abspath(local_path + 'fifty_abnormal_sample')
-    file_path = os.path.abspath(local_path + 's_submit_code\\user_' + str(rec["user_id"]) + "\\" + str(rec["path_idx"]) + '_unzip')
+    suffix = "\\" + 'user_' + str(rec["user_id"]) + "\\" + str(rec["path_idx"]) + '_unzip'
+    file_path = os.path.abspath(local_path + 's_submit_code' + suffix)
     path_dir = os.listdir(file_path)
-    to_path = sd + "\\" + 'user_' + str(rec["user_id"]) + "\\" + str(rec["path_idx"]) + '_unzip'
+    to_path = sd + suffix
 
     if os.path.exists(file_path):
         shutil.copytree(file_path, to_path)
@@ -29,9 +31,10 @@ data = list(dict.values(data))
 
 for rec in data:
     sd = os.path.abspath(local_path + 'fifty_valid_sample')
-    file_path = os.path.abspath(local_path + 's_submit_code\\user_' + str(rec["user_id"]) + "\\" + str(rec["path_idx"]) + '_unzip')
+    suffix = "\\" + 'user_' + str(rec["user_id"]) + "\\" + str(rec["path_idx"]) + '_unzip'
+    file_path = os.path.abspath(local_path + 's_submit_code' + suffix)
     path_dir = os.listdir(file_path)
-    to_path = sd + "\\" + 'user_' + str(rec["user_id"]) + "\\" + str(rec["path_idx"]) + '_unzip'
+    to_path = sd + suffix
 
     if os.path.exists(file_path):
         shutil.copytree(file_path, to_path)
